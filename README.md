61267009c9e5b0f540426efd6c18073b6f28a41d
Nick Clyde <nick@clyde.tech>
2023-04-25 22:31:52 -0700

    Set up initial lyrics endpoint

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-# ChatGPT plugins quickstart
+# ChatGPT Genius Plugin
 
-Get a Todo list ChatGPT plugin up and running in under 5 minutes using Python. If you do not already have plugin developer access, please [join the waitlist](https://openai.com/waitlist/plugins).
+Get song meanings, write song parodies, learn about artists and more with this ChatGPT plugin!  
+  
+If you do not already have plugin developer access, please [join the waitlist](https://openai.com/waitlist/plugins).
 
 ## Setup
 
@@ -13,7 +15,7 @@ pip install -r requirements.txt
 To run the plugin, enter the following command:
 
 ```bash
-python main.py
+uvicorn main:app --reload
 ```
 
 Once the local server is running:
@@ -22,7 +24,7 @@ Once the local server is running:
 2. In the Model drop down, select "Plugins" (note, if you don't see it there, you don't have access yet).
 3. Select "Plugin store"
 4. Select "Develop your own plugin"
-5. Enter in `localhost:5003` since this is the URL the server is running on locally, then select "Find manifest file".
+5. Enter in `localhost:8000` since this is the URL the server is running on locally, then select "Find manifest file".
 
 The plugin should now be installed and enabled! You can start with a question like "What is on my todo list" and then try adding something to it as well! 
 
--- main.py
+++ main.py
@@ -1,56 +1,53 @@
-import json
+from fastapi import FastAPI
+from fastapi.responses import Response
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+import functools
+import io
+import os
+from pydantic import BaseModel
+import yaml
+from lyricsgenius import Genius
 
-import quart
-import quart_cors
-from quart import request
 
-app = quart_cors.cors(quart.Quart(__name__), allow_origin="https://chat.openai.com")
+app = FastAPI()
+genius = Genius()
 
-# Keep track of todo's. Does not persist if Python session is restarted.
-_TODOS = {}
+# Serve static files in .well-known and assets
+app.mount("/.well-known", StaticFiles(directory=".well-known"), name="static")
+app.mount("/assets", StaticFiles(directory="assets"), name="static")
 
-@app.post("/todos/<string:username>")
-async def add_todo(username):
-    request = await quart.request.get_json(force=True)
-    if username not in _TODOS:
-        _TODOS[username] = []
-    _TODOS[username].append(request["todo"])
-    return quart.Response(response='OK', status=200)
+# CORS config
+origins = [
+    "http://localhost:8000",
+    "https://chat.openai.com",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/todos/<string:username>")
-async def get_todos(username):
-    return quart.Response(response=json.dumps(_TODOS.get(username, [])), status=200)
 
-@app.delete("/todos/<string:username>")
-async def delete_todo(username):
-    request = await quart.request.get_json(force=True)
-    todo_idx = request["todo_idx"]
-    # fail silently, it's a simple plugin
-    if 0 <= todo_idx < len(_TODOS[username]):
-        _TODOS[username].pop(todo_idx)
-    return quart.Response(response='OK', status=200)
+@app.get("/")
+async def root():
+    return {"message": "Welcome to the ChatGPT Genius Plugin!"}
 
-@app.get("/logo.png")
-async def plugin_logo():
-    filename = 'logo.png'
-    return await quart.send_file(filename, mimetype='image/png')
 
-@app.get("/.well-known/ai-plugin.json")
-async def plugin_manifest():
-    host = request.headers['Host']
-    with open("./.well-known/ai-plugin.json") as f:
-        text = f.read()
-        return quart.Response(text, mimetype="text/json")
+# Get song lyrics
+@app.get("/lyrics")
+async def get_lyrics(song: str, artist: str):
+    song = genius.search_song(song, artist)
+    return {"lyrics": song.lyrics}
 
-@app.get("/openapi.yaml")
-async def openapi_spec():
-    host = request.headers['Host']
-    with open("openapi.yaml") as f:
-        text = f.read()
-        return quart.Response(text, mimetype="text/yaml")
 
-def main():
-    app.run(debug=True, host="0.0.0.0", port=5003)
-
-if __name__ == "__main__":
-    main()
+# additional yaml version of openapi.json
+@app.get("/openapi.yaml", include_in_schema=False)
+@functools.lru_cache()
+def read_openapi_yaml() -> Response:
+    openapi_json = app.openapi()
+    yaml_s = io.StringIO()
+    yaml.dump(openapi_json, yaml_s)
+    return Response(yaml_s.getvalue(), media_type="text/yaml")
